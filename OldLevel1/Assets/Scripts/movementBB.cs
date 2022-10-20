b69437ecf191b229de154031d5e805570afb6776
Alvin Alagos <alagos_alvin@hotmail.com>
2022-10-20 19:56:53 -0400

    coverterd level 1 to version 2020.3.32f1

---
+++ OldLevel1/Assets/Scripts/movementBB.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementBB : MonoBehaviour
+{   
+    public Rigidbody rb;
+    public Transform cam;
+    public float speed = 500f;
+    public float jump = 20f;
+
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+    private float xInput, zInput;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        zInput = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(xInput, 0f, zInput).normalized;
+
+        if(direction.magnitude >= 0.1f){
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            rb.AddForce(moveDir.normalized * speed *Time.deltaTime);
+        }
+       
+        if (Input.GetKeyDown("space"))
+        {
+            rb.AddForce(new Vector3(0,jump,0), ForceMode.Impulse);
+        }
+
+    }
+
+
+}
