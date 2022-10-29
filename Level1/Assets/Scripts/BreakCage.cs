adb04c239128c97fa590faee2ed1da38128d667f
Denmar Ermitano <ermitano.den@gmail.com>
2022-10-29 03:37:57 -0400

    added platforms on top of pigs

--- Level1/Assets/Scripts/BreakCage.cs
+++ Level1/Assets/Scripts/BreakCage.cs
@@ -32,14 +32,16 @@ public class BreakCage : MonoBehaviour
         //needs to check velocity BEFORE collision, right at the moment of impact before it decelerates to 0
         float speed = velocityBeforePhysicsUpdate.y;
         
+        Debug.Log(speed);
+
         //falling at arbitrary speed. can/will be changed later.
         //
         //
-        if (speed < -5.0 && other.gameObject != beefyBoy && this.gameObject.activeInHierarchy) {
+        if (speed < -4.0 && other.gameObject != beefyBoy && this.gameObject.activeInHierarchy) {
 
             ContactPoint contact = other.contacts[0];
             Vector3 pos = contact.point;
-            Instantiate(explosion, pos, Quaternion. Euler(0, 0, 0));
+            Instantiate(explosion, pos, Quaternion. Euler(-90, 0, 0));
 
             //disables cage
             this.gameObject.SetActive(false);
