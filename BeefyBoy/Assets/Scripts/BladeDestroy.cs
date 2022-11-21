a5a047c46416055ae3f8b75d0f165f62d01823ce
gracexia02 <70624629+gracexia02@users.noreply.github.com>
2022-11-21 02:10:48 -0500

    create blade health bar (#96)

--- BeefyBoy/Assets/Scripts/BladeDestroy.cs
+++ BeefyBoy/Assets/Scripts/BladeDestroy.cs
@@ -1,20 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BladeDestroy : MonoBehaviour
 {
     public GameObject explodeEffect;
+     [SerializeField]
+    public GameObject HealthBarplane;
+    float Bladehealth = 1f ; 
     
     void Start()
     {
         
+        // Debug.Log("health:" + Bladehealth);
     }
 
     
     void Update()
     {
-        
+        if (Bladehealth == 0f){
+            // stopBelt();
+        }
     }
 
     private void OnTriggerEnter(Collider other) {
@@ -24,5 +31,15 @@ public class BladeDestroy : MonoBehaviour
         GameObject node = Instantiate(explodeEffect, null);
         node.transform.position = other.transform.position;
 
+        if(other.gameObject.name.Equals("pot(Clone)") || other.gameObject.name.Equals("meatTenderizer(Clone)" ) ){
+            Bladehealth -= 0.2f;
+            HealthBarplane.GetComponent<Image>().fillAmount = Bladehealth ;
+            // HealthBarplane.GetComponent<Image>().fillAmount -= 0.2f;
+            Debug.Log("health:" + Bladehealth);
+            
+        }
+
     }
+
+    
 }
