3a5c4a6645c760619f1804d745612a515cf4267d
Jiahui Xia <xiajiahui2002@163.com>
2022-11-26 00:34:46 -0500

    fix health bar for blade and beefyboy, and added jump damage for beefyboy

--- BeefyBoy/Assets/Scripts/BladeDestroy.cs
+++ BeefyBoy/Assets/Scripts/BladeDestroy.cs
@@ -8,8 +8,11 @@ public class BladeDestroy : MonoBehaviour
     public GameObject explodeEffect;
     [SerializeField]
     public GameObject HealthBarplane;
+    [SerializeField]
+    public GameObject bladeText;
     float Bladehealth = 1f ; 
     public Material glass;
+     
 
 
     void Start()
@@ -25,6 +28,8 @@ public class BladeDestroy : MonoBehaviour
             // stopBelt();
             exitDoor.GetComponent<Renderer>().material = glass;
         }
+
+        disableBladeText();
     }
 
     private void OnTriggerEnter(Collider other) {
@@ -34,7 +39,8 @@ public class BladeDestroy : MonoBehaviour
         GameObject node = Instantiate(explodeEffect, null);
         node.transform.position = other.transform.position;
 
-        if(other.gameObject.name.Equals("pot(Clone)") || other.gameObject.name.Equals("meatTenderizer(Clone)" ) ){
+        if(other.gameObject.name.Equals("potStew(Clone)") || other.gameObject.name.Equals("meatTenderizer(Clone)") 
+            || other.gameObject.name.Equals("pan(Clone)") || other.gameObject.name.Equals("can(Clone)") ){
             Bladehealth -= 0.2f;
             HealthBarplane.GetComponent<Image>().fillAmount = Bladehealth ;
             // HealthBarplane.GetComponent<Image>().fillAmount -= 0.2f;
@@ -44,9 +50,26 @@ public class BladeDestroy : MonoBehaviour
 
     }
 
+    void disableBladeText(){
+        // Debug.Log("disable function be called");
+        if(checkBladeHealthIsZore()) {
+            bladeText.SetActive(false);
+        }
+        
+    }
+
     void openDoorAnimation()
     {
         GetComponent<Renderer>().material = LoadResourceAsMaterial("Assets/BossLevelAsset/Materials/Glass.mat");
+    }
+
+    private bool checkBladeHealthIsZore(){
+        const float EPSINON = 0.000001f; 
+        if(( Bladehealth >= -EPSINON ) && ( Bladehealth <= EPSINON )) {
+            return true;
+        }else{
+            return false;
+        }
     }
 
     Material LoadResourceAsMaterial(string resourcePath){
